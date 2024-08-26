4e025e2ec779280f2513a9d94da1097932b0707a
Zach Bagnall <zach@y3m.net>
2024-08-25 18:19:58 -0600

    favicon?

--- src/App.js
+++ src/App.js
@@ -61,12 +61,13 @@ export default function App() {
       setMapReady(true);
 
       if ("geolocation" in navigator) {
-        // delay?
-        navigator.geolocation.getCurrentPosition((position) => {
-          let loc = {lat:position.coords.latitude, lng:position.coords.longitude};
-          saveDefaultLoc(loc);
-          setLocation(loc);
-        });      
+        window.setTimeout(() => {
+          navigator.geolocation.getCurrentPosition((position) => {
+            let loc = {lat:position.coords.latitude, lng:position.coords.longitude};
+            saveDefaultLoc(loc);
+            setLocation(loc);
+          });
+        }, 5000);
       }
     });
   });
